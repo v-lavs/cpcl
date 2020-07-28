a5b695a66614cc168e7f4c7e4a1f32d5808f7357
i_lavs <ihorl@titanium-labs.com>
2020-07-29 00:58:44 +0300

    added sliders markup

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,6 @@
  **/
 
 $(document).ready(function () {
-
     /**
      * HEADER SCROLL
      */
@@ -42,18 +41,72 @@ $(document).ready(function () {
     });
 
 
-   // PARALAX
-   //  $('.mouse-parallax').on('mousemove', (e)=>{
-   //      const x = e.pageX / $(window).width();
-   //      const y = e.pageY / $(window).height();
-   //
-   //      $('.mouse-parallax__bg').css(
-   //          'transform',
-   //          'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)'
-   //      );
-   //      $('.mouse-parallax__bg-2').css(
-   //          'transform',
-   //          'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
-   //      );
-   //        });
+    // PARALAX
+    $('.mouse-parallax').on('mousemove', (e) => {
+        const x = e.pageX / $(window).width();
+        const y = e.pageY / $(window).height();
+
+        $('.mouse-parallax__bg').css(
+            'transform',
+            'translate(-' + x * 35 + 'px, -' + y * 25 + 'px)'
+        );
+        $('.mouse-parallax__bg-2').css(
+            'transform',
+            'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'
+        );
+    });
+
+    // Banner slider
+    var bannerSlider;
+    var instructionSlider;
+    var headAcheTypesSlider;
+
+    function slidersInit () {
+        if ($(window).width() <= 767) {
+            if(!bannerSlider) {
+                bannerSlider = new Swiper('.banner-slider');
+            }
+            if(!instructionSlider) {
+                instructionSlider = new Swiper('#instructionSlider');
+            }
+            if(!headAcheTypesSlider) {
+                headAcheTypesSlider = new Swiper('#headAcheTypesSlider');
+            }
+        } else {
+            if(bannerSlider) {
+                bannerSlider.destroy(true, true);
+                bannerSlider = null;
+            }
+            if(instructionSlider) {
+                instructionSlider.destroy(true, true);
+                instructionSlider = null;
+            }
+            if(headAcheTypesSlider) {
+                headAcheTypesSlider.destroy(true, true);
+                headAcheTypesSlider = null;
+            }
+        }
+    }
+
+    slidersInit();
+
+    // PAIN SYMPTOMS Hover block
+
+    var $imagesList = $('#pain-syndrom-imgs .link-syndrom');
+    var $syndromsItems = $('.pain-syndrome-list__item');
+
+    $imagesList.hover(function () {
+        $syndromsItems.removeClass('active-syndrom');
+        $imagesList.removeClass('active');
+        $(this).addClass('active');
+        $('[data-id='+ $(this).data('for') +']').addClass('active-syndrom');
+        console.log($(this).data('for'));
+    });
+
+    $(window).on('resize', function() {
+        slidersInit();
+    });
+
 });
+
+
